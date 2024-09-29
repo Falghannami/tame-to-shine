379e2040845232a9e8d0cb2e1531fc48314b77d4
Falghannami <falghannami24@twq.idserve.net>
2024-09-29 09:37:22 +0300

    Initial Commit

---
+++ tame to shine/tame_to_shineApp.swift
@@ -0,0 +1,17 @@
+//
+//  tame_to_shineApp.swift
+//  tame to shine
+//
+//  Created by فالحه الغنامي on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct tame_to_shineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tame to shineTests/tame_to_shineTests.swift
@@ -0,0 +1,17 @@
+//
+//  tame_to_shineTests.swift
+//  tame to shineTests
+//
+//  Created by فالحه الغنامي on 26/03/1446 AH.
+//
+
+import Testing
+@testable import tame_to_shine
+
+struct tame_to_shineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
